778f6af7f82918b938268fbc5f36ef07bca1a278
William Monroy <william_monroy777@hotmail.com>
2020-11-21 19:29:18 -0500

    compatibilidad SO

---
+++ Util.h
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+
+using namespace std;
+
+void clear(string SO){
+    if(SO=="Windows"){
+        system("cls");
+    }else{
+        system("clear");
+    }
+}
+
+void pause(string SO){
+    if(SO=="Windows"){
+        system("pause");
+    }else{
+        cout << "Presione enter para continuar...";
+        string temp;
+        cin>>temp;
+    }
+}
+/*
+void color(int num){
+    string cmd = to_string(num);
+    char const *cmd2 = cmd.c_str();
+    system(strcat("color ",cmd2));   
+}  */
+
--- main.cpp
+++ main.cpp
@@ -1,14 +1,36 @@
 #include <iostream>
+#include <stdlib.h> 
+
+#include "Util.h"
 
 using namespace std;
 
+
+
 int main() {
 
+    string SO = "";
+
+    #if defined(_WIN64)
+        cout << "Estoy en Windows de 64 bits.\n" << endl;
+        SO = "Windows";
+    #elif defined(__linux__)
+        cout << "Estoy en Linux." << endl;
+        SO = "Linux";
+    #elif defined(__MACH__)
+        cout << "Estoy en Mac." << endl;
+        SO = "Mac";
+    #endif
 
 
+    pause(SO);
+    clear(SO);
 
 
 
 
     return 0;
 }
+
+
+
